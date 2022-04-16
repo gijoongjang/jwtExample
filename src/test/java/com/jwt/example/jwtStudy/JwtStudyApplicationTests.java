c8feeb0e1a9f13ff72d860e4fbfb8b08aa425078
giJoongJang <rlwnd0000@gmail.com>
2022-04-16 17:44:26 +0900

    Initial commit

---
+++ src/test/java/com/jwt/example/jwtStudy/JwtStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.example.jwtStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
